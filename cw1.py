7c85975c3e31800384e7aa5fca4c96761f8b2f10
Bh00fie <Bh00fie@gmail.com>
2023-10-24 12:10:48 +0100

    defined general material properties

--- cw1.py
+++ cw1.py
@@ -4,14 +4,20 @@ import numpy as np
 #Data given:
 S3 = 0.2
 G12 = 1
+WFibre = 0.6 #Fibre weight Fraction
 #Carbon
-pCarbon = 1870 #km/m^3
-ECarbon = 310 #GN/m^2
-vCarbon = 0.23
+pFibre = 1870 #km/m^3
+EFibre = 310 #GN/m^2
+vFibre = 0.23
 #Epoxy
-pEpoxy = 1200 #km/m^3
-EEpoxy = 2.4 #GN/m^2
-vEpoxy = 0.33
+pMatrix = 1200 #km/m^3
+EMatrix = 2.4 #GN/m^2
+vMatrix = 0.33
+
+WMatrix = 1 - WFibre
+VFibre = (WFibre/pFibre)/((WFibre/pFibre)+(WMatrix/pMatrix))
+VMatrix = 1 - VFibre
+
 
 
 
