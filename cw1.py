bd63bcc6cdd78ee698df62766c7fd49ede1d610a
Bh00fie <Bh00fie@gmail.com>
2023-11-03 14:55:27 +0000

    cleared the code and found a way to calculate Ex and Ey correctly

--- cw1.py
+++ cw1.py
@@ -10,67 +10,64 @@ thickness = 0.001 #mm For each layer
 # Laminate Configuration for Question 1
 laminate_config_q1 = [0, 90, 90, 0]
 nLayers = len(laminate_config_q1)
-# Calculating the bottom and top thickness
+
+# Calculating the bottom and top thickness of the laminate
 position = [-nLayers * thickness / 2 + i * thickness for i in range(nLayers + 1)]
 
-# Created an empty list to store Qbar matrices for each angle
+# Creating an empty list to store Qbar, A1, B1, D1 matrices for each angle
 Qbar_list = []
 A1_list = []
 B1_list = []
 D1_list = []
 
-for i in range(nLayers):  # Use a loop to keep track of the current layer
-    angle = laminate_config_q1[i]
-    theta = angle * (np.pi / 180)  # Calculate theta based on the angle
+# Use a loop to keep track of the current layer
+for i in range(nLayers):  
+    angle = laminate_config_q1[i] # Changing index/angle for every loop iteration
+    theta = angle * (np.pi / 180)  # Calculate theta based on the angle converted 
     
-    #Carbon
-    pFibre = 1870 #km/m^3
-    EFibre = 310 #GN/m^2
-    vFibre = 0.23
+    # Defining Carbon Fibre Properties
+    pFibre = 1870 #km/m^3 - Density
+    EFibre = 310 #GN/m^2 - Young's Modulus
+    vFibre = 0.23 # - Poisson Ratio
     #Epoxy
-    pMatrix = 1200 #km/m^3
-    EMatrix = 2.4 #GN/m^2
-    vMatrix = 0.33
+    pMatrix = 1200 #km/m^3 - Density
+    EMatrix = 2.4 #GN/m^2 - Young's Modulus
+    vMatrix = 0.33 # - Poisson Ratio
 
-    WMatrix = 1 - WFibre
-    VFibre = (WFibre/pFibre)/((WFibre/pFibre)+(WMatrix/pMatrix))
-    VMatrix = 1 - VFibre
-    EFibre = 310
-    vFibre = 0.23
-    VFibre = 0.49
-    EMatrix = 2.4
-    vMatrix = 0.33
-    VMatrix = 1 - VFibre
-
-    #Rule of mixture
-    #Young Modulus
-    E1 = EMatrix*VMatrix + EFibre*VFibre
+    WMatrix = 1 - WFibre # Calculating the Weight Fraction value of the Matrix
+    VFibre = (WFibre/pFibre)/((WFibre/pFibre)+(WMatrix/pMatrix)) # Calculating the Volume Fraction of the Fibre
+    VMatrix = 1 - VFibre # Calculating the Volume Fraction of the Matrix
     
-    E2 = (EFibre*EMatrix)/((VFibre*EMatrix)+(VMatrix*EFibre))
+    # EFibre = 310 
+    # vFibre = 0.23
+    # VFibre = 0.49
+    # EMatrix = 2.4
+    # vMatrix = 0.33
+    # VMatrix = 1 - VFibre
 
-    #Shear Modulus
-    GFibre = EFibre/(2*(1+vFibre))
-    GMatrix = EMatrix/(2*(1+vMatrix))
-    
-    # print(EMatrix, vMatrix)
-    G12 = (GFibre*GMatrix)/((GFibre*VMatrix) + (GMatrix*VFibre))
-    # print(GMatrix, GFibre, VMatrix, VFibre)
+    # RULE OF MIXTURE
+    #Young Modulus Calculation
+    E1 = EMatrix*VMatrix + EFibre*VFibre  # Young's Modulus in the 1 direction
+    E2 = (EFibre*EMatrix)/((VFibre*EMatrix)+(VMatrix*EFibre)) # Young's Modulus in the 2 direction
+
+    #Shear Modulus Calculation
+    GFibre = EFibre/(2*(1+vFibre)) # Shear Modulus of the Fibre
+    GMatrix = EMatrix/(2*(1+vMatrix)) # Shear Modulus of the Matrix
+    G12 = (GFibre*GMatrix)/((GFibre*VMatrix) + (GMatrix*VFibre)) # Shear Modulus of the Laminate
 
     #Major Poisson Ratio
     v12 = vFibre*VFibre + vMatrix*VMatrix
-    # print(EFibre, vFibre, VFibre, EMatrix, vMatrix, VMatrix, GFibre, GMatrix, E1, E2, v12, G12) 
-    #HT Method
-    nE = ((EFibre/EMatrix)-1)/((EFibre/EMatrix) + S3)
-    E2 = EMatrix * ((1+(S3*nE*VFibre))/(1-(nE*VFibre)))
-    
+ 
+    # HALPIN TSAI 
+    nE = ((EFibre/EMatrix)-1)/((EFibre/EMatrix) + S3) 
+    E2 = EMatrix * ((1+(S3*nE*VFibre))/(1-(nE*VFibre))) # Young's Modulus in the 2 direction
+     
     nG = ((GFibre/GMatrix) - 1)/((GFibre/GMatrix) + 1)
-    G12 = GMatrix * ((1 + S12*nG*VFibre)/(1 - nG*VFibre))
+    G12 = GMatrix * ((1 + S12*nG*VFibre)/(1 - nG*VFibre)) # Shear Modulus of the Laminate
 
-    #Minor Poisson Ration
-    v21 = v12 *(E2/E1)
+    v21 = v12 *(E2/E1)  # Minor Poisson Ration
 
-    # print(E1, E2, v12, v21, G12)
-    #Calculating Qmatrix
+    # Qmatrix
     Q11 = E1 / (1 - v12 * v21)
     Q22 = E2 / (1 - v12 * v21)
     Q12 = v12 * E2 / (1 - v12 * v21)
@@ -79,7 +76,7 @@ for i in range(nLayers):  # Use a loop to keep track of the current layer
                 [Q12, Q22, 0],
                 [0, 0, Q66]])
 
-    # Calculating Tmatrix
+    # Tmatrix
     T11 = np.cos(theta) ** 2
     T12 = np.sin(theta) ** 2
     T13 = 2 * np.sin(theta) * np.cos(theta)
@@ -94,15 +91,14 @@ for i in range(nLayers):  # Use a loop to keep track of the current layer
                 [T21, T22, T23],
                 [T31, T32, T33]])
 
-    #Calculate the inverse of T matrix
+    # Inverse of T matrix
     T_inv = np.linalg.inv(T)
 
-    #Calculate the transpose of the inverse matrix
+    # Transpose of the inverse matrix
     T_inv_transpose = np.transpose(T_inv)
 
-    #Calculating Qbar Matrix
+    # Qbar Matrix calculate dot product with T_Inv, Q, T_inv_transpose
     Qbar = T_inv @ Q @ T_inv_transpose
-    # Qbar = np.round(Qbar, 4)
 
     # Converting the NumPy array to a regular Python list
     Qbar_list.append(Qbar.tolist())
@@ -128,85 +124,64 @@ for i, Qbar_matrix in enumerate(Qbar_list):
     print()
     
 # Print the A1 matrices in a human-readable format
-# for i, B1_matrix in enumerate(B1_list):
-#     print(f"A1 for angle {laminate_config_q1[i]} degrees:")
-#     print(B1_matrix)
-#     print()
+for i, B1_matrix in enumerate(B1_list):
+    print(f"A1 for angle {laminate_config_q1[i]} degrees:")
+    print(B1_matrix)
+    print()
 
-A = np.sum(A1_list, axis=0)* 1
-B = np.sum(B1_list, axis=0)* 1
-D = np.sum(D1_list, axis=0)* 1
+# Defining one single A, B, D matrix for the laminate (through summing A, B, D for each lamina)
+A = np.sum(A1_list, axis=0)* 1e9
+B = np.sum(B1_list, axis=0)* 1e9
+D = np.sum(D1_list, axis=0)* 1e9
 
-# print("A:")
-# print(A)
-print("B:")
-print(B)
-# print("D:")
-# print(D)
-
-# Concatenate the A, B, and D matrices in the specified order
+# Concatenating the A, B, and D matrices in the specified order
 ABBD = np.block([[A, B], [B, D]])
 
-# Set numpy print options to display numbers without scientific notation
-# np.set_printoptions(suppress=True, formatter={'float': lambda x: '0' if x == 0.0 else '{:0.3f}'.format(x)})
+# Set numpy print options to display numbers without scientific notation, NOT ROUNDING
+np.set_printoptions(suppress=True, formatter={'float': lambda x: '0' if x == 0.0 else '{:0.3f}'.format(x)})
 
-# Print the ABBD matrix
-print("ABBD Matrix:")
-print(ABBD)
+print("ABBD Matrix:") # Printing the ABBD matrix
+print(ABBD) # Printing the ABBD matrix
 
-# Inverse ABBD Matrix
-ABBD_inv = np.linalg.inv(ABBD)
+# Inverse of ABBD Matrix
+ABBD_inv = np.linalg.inv(ABBD) 
 
-# Print the ABBD_inv matrix
-print("ABBD_inv Matrix:")
-print(ABBD_inv)
+print("ABBD_inv Matrix:") # Printing the ABBD_inv matrix
+print(ABBD_inv) # Printing the ABBD_inv matrix
 
-Nx = 1
-Ny = 1
-Nxy = 1
-Mx = 1
-My = 1
-Mxy = 1
+# Define the different directions
+directions = ["Ex", "Ey", "Exy"]
 
-NM = ([[Nx],[Ny],[Nxy],[Mx],[My],[Mxy]])
-# print(NM)
+for direction in directions:
+    # Initialize the NM Matrix
+    NM = [0] * 6
+    
+    if direction == "Ex":
+        NM[0] = 1
+    elif direction == "Ey":
+        NM[1] = 1
+    elif direction == "Exy":
+        NM[2] = 1
 
-# Calculate the [ϵx, ϵy, γxy, κx, κy, κxy] vector
-result = np.dot(ABBD_inv, NM)
+    NM = np.array(NM).reshape(6, 1)
 
-# Print the result
-# print("Resulting [ex, ey, yxy, kx, ky, kxy] vector:")
-# print(result)
+    # Calculate the strain in different directions -> [ϵx, ϵy, γxy, κx, κy, κxy] vector
+    result = np.dot(ABBD_inv, NM)
 
-ex = result[0]
-ey = result[1]
-yxy = result[2]
-kx = result[3]
-ky = result[4]
-kxy = result[5]
+    # Assigning each strain to its value in the result matrix
+    ex, ey, yxy, kx, ky, kxy = result
 
-# print(ex)
-# print(ey)
-# print(yxy)
-# print(kx)
-# print(ky)
-# print(kxy)
+    if direction == "Ex":
+        Sx = ((A[0, 0] * ex + A[0, 1] * ey + A[0, 2] * yxy + B[0, 0] * kx + B[0, 1] * ky + B[0, 2] * kxy)) / (nLayers * thickness)
+        Ex = Sx / ex
+        print(f"Ex: {Ex}")
+    elif direction == "Ey":
+        Sy = ((A[1, 0] * ex + A[1, 1] * ey + A[1, 2] * yxy + B[1, 0] * kx + B[1, 1] * ky + B[1, 2] * kxy)) / (nLayers * thickness)
+        Ey = Sy / ey
+        print(f"Ey: {Ey}")
+    elif direction == "Exy":
+        Sxy = ((A[2, 0] * ex + A[2, 1] * ey + A[2, 2] * yxy + B[2, 0] * kx + B[2, 1] * ky + B[2, 2] * kxy)) / (nLayers * thickness)
+        Gxy = Sxy / yxy
+        print(f"Gxy: {Gxy}")
 
-# Sx = ((A[0,0]*ex + A[0,1]*ey + A[0,2]*yxy + B[0,0]*kx + B[0,1]*ky + B[0,2]*kxy))/(nLayers*thickness)
-# Sy = ((A[1,0]*ex + A[1,1]*ey + A[1,2]*yxy + B[1,0]*kx + B[1,1]*ky + B[1,2]*kxy))/(nLayers*thickness)
-# Sxy = ((A[2,0]*ex + A[2,1]*ey + A[2,2]*yxy + B[2,0]*kx + B[2,1]*ky + B[2,2]*kxy))/(nLayers*thickness)
-# Sx = Nx/(nLayers*thickness)
-# Sy = Ny/(nLayers*thickness)
-Sxy = Nxy/(nLayers*thickness)
-# print(Sx)
-# Ex = Sx/ex
-# Ey = Sy/ey
-Gxy = Sxy/yxy
-# print(Ex)
-# print(Ey)
-print(Gxy)
-
-Ex = (1/(nLayers*thickness))*(A[0,0] - (((A[0,1])**2)/A[1,1]))
-print(Ex)
-Ey = (1/(nLayers*thickness))*(A[1,1] - (((A[0,1])**2)/A[0,0]))
-print(Ey)
+# REMEMBER UNITS
