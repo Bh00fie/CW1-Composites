fa1177262a6945fbb39fd6c23f812f9932eecc06
Bh00fie <Bh00fie@gmail.com>
2023-11-03 16:33:19 +0000

    added print function to show results in a aestatically positive way for a report and for the user

--- cw1.py
+++ cw1.py
@@ -5,11 +5,14 @@ import numpy as np
 S3 = 0.2
 S12 = 1
 WFibre = 0.6 #Fibre weight Fraction
-thickness = 0.001 #mm For each layer
+thickness = 0.001 #m For each layer
 
 # Laminate Configuration for Question 1
 laminate_config_q1 = [0, 90, 90, 0]
 nLayers = len(laminate_config_q1)
+
+print(f'The laminate configuration for this laminate is: {laminate_config_q1}')
+print(f'The thickness of the laminate is: {nLayers*thickness} m or {nLayers*thickness*1e3} mm, each laminate in the configuration has the same thickness, thus: {thickness} m')
 
 # Calculating the bottom and top thickness of the laminate
 position = [-nLayers * thickness / 2 + i * thickness for i in range(nLayers + 1)]
@@ -35,29 +38,24 @@ for i in range(nLayers):
     vMatrix = 0.33 # - Poisson Ratio
 
     WMatrix = 1 - WFibre # Calculating the Weight Fraction value of the Matrix
-    VFibre = (WFibre/pFibre)/((WFibre/pFibre)+(WMatrix/pMatrix)) # Calculating the Volume Fraction of the Fibre
+    VFibre = np.round((WFibre / pFibre) / ((WFibre / pFibre) + (WMatrix / pMatrix)), 3)  # Calculating the Volume Fraction of the Fibre, rounded to 3 digits using np.round
     VMatrix = 1 - VFibre # Calculating the Volume Fraction of the Matrix
-    
-    # EFibre = 310 
-    # vFibre = 0.23
-    # VFibre = 0.49
-    # EMatrix = 2.4
-    # vMatrix = 0.33
-    # VMatrix = 1 - VFibre
 
     # RULE OF MIXTURE
     #Young Modulus Calculation
     E1 = EMatrix*VMatrix + EFibre*VFibre  # Young's Modulus in the 1 direction
-    E2 = (EFibre*EMatrix)/((VFibre*EMatrix)+(VMatrix*EFibre)) # Young's Modulus in the 2 direction
+    E2_RoM = (EFibre*EMatrix)/((VFibre*EMatrix)+(VMatrix*EFibre)) # Young's Modulus in the 2 direction
 
     #Shear Modulus Calculation
     GFibre = EFibre/(2*(1+vFibre)) # Shear Modulus of the Fibre
     GMatrix = EMatrix/(2*(1+vMatrix)) # Shear Modulus of the Matrix
-    G12 = (GFibre*GMatrix)/((GFibre*VMatrix) + (GMatrix*VFibre)) # Shear Modulus of the Laminate
-
+    G12_RoM = (GFibre*GMatrix)/((GFibre*VMatrix) + (GMatrix*VFibre)) # Shear Modulus of the Laminate
+    
     #Major Poisson Ratio
     v12 = vFibre*VFibre + vMatrix*VMatrix
- 
+    #Minor Poisson Ratio
+    v21_RoM = v12 *(E2_RoM/E1)  # Minor Poisson Ration
+    
     # HALPIN TSAI 
     nE = ((EFibre/EMatrix)-1)/((EFibre/EMatrix) + S3) 
     E2 = EMatrix * ((1+(S3*nE*VFibre))/(1-(nE*VFibre))) # Young's Modulus in the 2 direction
@@ -89,7 +87,7 @@ for i in range(nLayers):
 
     T = np.array([[T11, T12, T13],
                 [T21, T22, T23],
-                [T31, T32, T33]])
+                [T31, T32, T33]]) 
 
     # Inverse of T matrix
     T_inv = np.linalg.inv(T)
@@ -98,7 +96,7 @@ for i in range(nLayers):
     T_inv_transpose = np.transpose(T_inv)
 
     # Qbar Matrix calculate dot product with T_Inv, Q, T_inv_transpose
-    Qbar = T_inv @ Q @ T_inv_transpose
+    Qbar = T_inv @ Q @ T_inv_transpose * 1e9
 
     # Converting the NumPy array to a regular Python list
     Qbar_list.append(Qbar.tolist())
@@ -115,46 +113,63 @@ for i in range(nLayers):
     D1 = (1/3)*Qbar * ((position[i + 1]**3) - (position[i]**3))
     D1_list.append(D1)
 
-    
-# Print the Qbar matrices in a human-readable format
-for i, Qbar_matrix in enumerate(Qbar_list):
-    print(f"Qbar for angle {laminate_config_q1[i]} degrees:")
-    for row in Qbar_matrix:
-        print(row)
-    print()
-    
-# Print the A1 matrices in a human-readable format
-for i, B1_matrix in enumerate(B1_list):
-    print(f"A1 for angle {laminate_config_q1[i]} degrees:")
-    print(B1_matrix)
-    print()
-
-# Defining one single A, B, D matrix for the laminate (through summing A, B, D for each lamina)
-A = np.sum(A1_list, axis=0)* 1e9
-B = np.sum(B1_list, axis=0)* 1e9
-D = np.sum(D1_list, axis=0)* 1e9
-
-# Concatenating the A, B, and D matrices in the specified order
-ABBD = np.block([[A, B], [B, D]])
+print('THE MATERIAL PROPERTIES ARE:')   
+print(f"Fibre Young's Modulus: {EFibre} GN/m^2")
+print(f"Fibre Poisson's Ratio: {vFibre} Kg/m^3")
+print(f"Fibre Volume Factor: {VFibre}")
+print(f"Matrix Young's Modulus: {EMatrix} GN/m^2")
+print(f"Matrix Poisson's Ratio: {vMatrix} Kg/m^3")
+print(f"Matrix Volume Factor: {VMatrix}")
+print(f"Fibre Shear Stress: {GFibre} GN/m^2")
+print(f"Matrix Shear Stress: {GMatrix} GN/m^2 \n")
+print('-----------------')
+print('\n Using the RULE OF MIXTURE:')
+print(f"E11 is: {E1} GN/m^2")
+print(f"E22 is: {E2_RoM} GN/m^2")
+print(f"v12 is: {v12}")
+print(f"v21 is: {v21_RoM}")
+print(f"G12 is: {G12_RoM} GN/m^2")
+print('\n Using HALPIN-TSAI:')
+print(f"E11 is: {E1} GN/m^2")
+print(f"E22 is: {E2} GN/m^2")
+print(f"v12 is: {v12}")
+print(f"v21 is: {v21}")
+print(f"G12 is: {G12} GN/m^2 \n")
+print('----------------- \n')
 
 # Set numpy print options to display numbers without scientific notation, NOT ROUNDING
 np.set_printoptions(suppress=True, formatter={'float': lambda x: '0' if x == 0.0 else '{:0.3f}'.format(x)})
 
-print("ABBD Matrix:") # Printing the ABBD matrix
-print(ABBD) # Printing the ABBD matrix
+# Print the Qbar matrices in a human-readable format
+for i, Qbar_matrix in enumerate(Qbar_list):
+    print(f"Qbar for angle {laminate_config_q1[i]} degrees in N/m^2:")
+    for row in Qbar_matrix:
+        print(row)
+    print()
+    
+# Defining one single A, B, D matrix for the laminate (through summing A, B, D for each lamina)
+A = np.sum(A1_list, axis=0)
+B = np.sum(B1_list, axis=0)
+D = np.sum(D1_list, axis=0)
+
+print(f'The A Matrix in N/m is: \n {A}')
+print(f'\nThe B Matrix in N is: \n {B}')
+print(f'\nThe D Matrix in Nm is: \n {D}')
+print(' \n----------------- \n')
+# Concatenating the A, B, and D matrices in the specified order
+ABBD = np.block([[A, B], [B, D]])
+print(f"ABBD Matrix: \n{ABBD} \n ") # Printing the ABBD matrix
 
 # Inverse of ABBD Matrix
 ABBD_inv = np.linalg.inv(ABBD) 
-
-print("ABBD_inv Matrix:") # Printing the ABBD_inv matrix
-print(ABBD_inv) # Printing the ABBD_inv matrix
+print(f"ABBD_inv Matrix: \n{ABBD_inv}") # Printing the ABBD_inv matrix
+print('\n ----------------- \n')
 
 # Define the different directions
 directions = ["Ex", "Ey", "Exy"]
 
 for direction in directions:
-    # Initialize the NM Matrix
-    NM = [0] * 6
+    NM = [0] * 6     # Initialize the NM Matrix
     
     if direction == "Ex":
         NM[0] = 1
@@ -165,23 +180,19 @@ for direction in directions:
 
     NM = np.array(NM).reshape(6, 1)
 
-    # Calculate the strain in different directions -> [ϵx, ϵy, γxy, κx, κy, κxy] vector
-    result = np.dot(ABBD_inv, NM)
+    result = np.dot(ABBD_inv, NM) # Calculate the strain in different directions -> [ϵx, ϵy, γxy, κx, κy, κxy] vector
 
-    # Assigning each strain to its value in the result matrix
-    ex, ey, yxy, kx, ky, kxy = result
+    ex, ey, yxy, kx, ky, kxy = result # Assigning each strain to its value in the result matrix
 
     if direction == "Ex":
         Sx = ((A[0, 0] * ex + A[0, 1] * ey + A[0, 2] * yxy + B[0, 0] * kx + B[0, 1] * ky + B[0, 2] * kxy)) / (nLayers * thickness)
         Ex = Sx / ex
-        print(f"Ex: {Ex}")
+        print(f"Ex: {Ex} N/m^2")
     elif direction == "Ey":
         Sy = ((A[1, 0] * ex + A[1, 1] * ey + A[1, 2] * yxy + B[1, 0] * kx + B[1, 1] * ky + B[1, 2] * kxy)) / (nLayers * thickness)
         Ey = Sy / ey
-        print(f"Ey: {Ey}")
+        print(f"Ey: {Ey} N/m^2")
     elif direction == "Exy":
         Sxy = ((A[2, 0] * ex + A[2, 1] * ey + A[2, 2] * yxy + B[2, 0] * kx + B[2, 1] * ky + B[2, 2] * kxy)) / (nLayers * thickness)
         Gxy = Sxy / yxy
-        print(f"Gxy: {Gxy}")
-
-# REMEMBER UNITS
+        print(f"Gxy: {Gxy} N/m^2 \n")
